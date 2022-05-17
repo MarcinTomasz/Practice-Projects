352f62b759c51a7f191c3639f20d11a39026557a
MarcinTomasz <58958990+MarcinTomasz@users.noreply.github.com>
2022-05-18 01:35:15 +0200

    Create gapminder_df.py

---
+++ gapminder_df.py
@@ -0,0 +1,37 @@
+#Code for one of the most famous visualizations.
+
+import plotly.express as px
+gapminder_df = px.data.gapminder()
+gapminder_df
+
+"""px.scatter(data_frame=gapminder_df,
+           x = 'gdpPercap',
+           y = 'lifeExp',
+           size = 'pop',
+           color = 'continent',
+           title = 'Life Span and Wealth 1952 - 2007',
+           labels = {'gdpPercap': 'Wealth',
+                    'lifeExp': 'Life Span'},
+           log_x = True,
+           range_y = [25, 95],
+           hover_name= 'country',
+           animation_frame= 'year',
+           height = 600,
+           size_max= 100)"""
+
+fig = px.scatter(data_frame=gapminder_df,
+           x = 'gdpPercap',
+           y = 'lifeExp',
+           size = 'pop',
+           color = 'continent',
+           title = 'Life Span and Wealth 1952 - 2007',
+           labels = {'gdpPercap': 'Wealth',
+                    'lifeExp': 'Life Span'},
+           log_x = True,
+           range_y = [25, 95],
+           hover_name= 'country',
+           animation_frame= 'year',
+           height = 600,
+           size_max= 100)
+
+fig.show()
